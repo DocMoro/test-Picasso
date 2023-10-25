2ef4ec43ff6f9f018fad5e67aa442b25b5c0533f
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-10-25 16:39:48 +0300

    fix: исправить роутинг

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import './index.css';
 import App from './app/App/components/App';
 import reportWebVitals from './reportWebVitals';
 
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
+
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </QueryClientProvider>
 );
 
