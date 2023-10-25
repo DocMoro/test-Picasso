a82410f347db028f404b992a1eabf43fd0ba7224
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-10-25 15:56:43 +0300

    Updates

--- src/pages/Main/components/Main/Main.js
+++
@@ -1,9 +0,0 @@
-import PostsList from "../../../../widgets/PostsList/components/PostsList/PostsList"
-
-export default function Main() {
-  return (
-    <main>
-      <PostsList />
-    </main>
-  )
-}
